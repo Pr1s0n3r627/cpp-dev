4fd7e8f0e28eac0678f244e0d8bcccbace868dce
Kshitij Sahdev <kshitijsahdev5@gmail.com>
2024-08-21 15:55:10 +0530

    o

---
+++ mergecort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int arr[] = {9, 5, 1, 8, 3, 2, 7, 6, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
